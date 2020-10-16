ba7dd9600b7fe22fb365faa5f530c914ff795d43
Kodie campbell <delur@kodiesLaptop.campbell.lab>
2020-10-16 16:36:52 -0500

    init the bot and test by posting first message

--- index.js
+++ index.js
@@ -4,3 +4,23 @@ const SlackBot = require('slackbots');
 const axios = require('axios');
 // dotenv loads env's to hide sensitive data 
 const dotenv = require('dotenv');
+
+
+// create bot and get the bot token
+const bot = new SlackBot({
+    token: `${process.env.BOT_TOKEN}`,
+    name: 'sidekick'
+
+});
+
+bot.on('start', () => {
+    const params = {
+        icon_emoji: ':robot_face:'
+    }
+
+    bot.postMessageToChannel(
+        'test',
+        'This is a test to see if i am on and working',
+        params
+    );
+});
