da66f1be31024f052205cf3dc741167fd8a7d53b
Kodie campbell <delur@kodiesLaptop.campbell.lab>
2020-10-16 15:07:17 -0500

    first commit dependencies installed

---
+++ index.js
@@ -0,0 +1,6 @@
+// require dependencies 
+const SlackBot = require('slackbots');
+// axios is an HTTP client https://github.com/axios/axios
+const axios = require('axios');
+// dotenv loads env's to hide sensitive data 
+const dotenv = require('dotenv');
